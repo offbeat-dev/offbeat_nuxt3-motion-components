7484f189177c72b250169ed2b0609f52c1720a7c
offbeat-dev <109253600+offbeat-dev@users.noreply.github.com>
2022-12-20 12:52:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# offbeat_nuxt3-motion-components
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-e8j5vo)
---
+++ components/AnimatedLink.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { animate, stagger } from 'motion';
+import SplitType from 'split-type';
+const link = ref(null);
+let splitText;
+
+onMounted(() => {
+  splitText = new SplitType(link.value, { types: 'chars' });
+});
+
+function animateHover() {
+  animate(
+    splitText.chars,
+    { y: '10px' },
+    { duration: 1, delay: stagger(0.05) }
+  );
+}
+
+function animateExit() {
+  animate(splitText.chars, { y: 0 }, { duration: 1, delay: stagger(0.05) });
+}
+</script>
+<template>
+  <a
+    ref="link"
+    class="link"
+    @mouseover="
+      {
+        animateHover();
+      }
+    "
+    @mouseleave="
+      {
+        animateExit();
+      }
+    "
+    href="/"
+    >Link</a
+  >
+</template>
+<style>
+.link:focus {
+  outline: 2px solid #000;
+}
+</style>
