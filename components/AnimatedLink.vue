4b66988139a1e4fb95a125d809551abf98b86f21
offbeat-dev <109253600+offbeat-dev@users.noreply.github.com>
2022-12-21 10:31:57 -0500

    add overflow effect and sequence

--- components/AnimatedLink.vue
+++ components/AnimatedLink.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { animate, stagger } from 'motion';
+import { timeline, animate, stagger } from 'motion';
 import SplitType from 'split-type';
 const link = ref(null);
 let splitText;
@@ -9,11 +9,25 @@ onMounted(() => {
 });
 
 function animateHover() {
-  animate(
-    splitText.chars,
-    { y: '10px' },
-    { duration: 1, delay: stagger(0.05) }
+  const sequence = [];
+  sequence.push(
+    [
+      splitText.chars,
+      { transform: 'translate(0, 100%)' },
+      { duration: 0.25, delay: stagger(0.05) },
+    ],
+    [
+      splitText.chars,
+      { transform: 'translate(0, -100%)' },
+      { duration: 0.0000000001 },
+    ],
+    [
+      splitText.chars,
+      { transform: 'translate(0, 0)' },
+      { duration: 0.25, delay: stagger(0.05) },
+    ]
   );
+  timeline(sequence);
 }
 
 function animateExit() {
@@ -41,5 +55,10 @@ function animateExit() {
 <style>
 .link:focus {
   outline: 2px solid #000;
+}
+.link {
+  display: block;
+  position: relative;
+  overflow: hidden;
 }
 </style>
