3a26b961d7de6fa10ebb2b5b39e3a08192debf84
offbeat-dev <109253600+offbeat-dev@users.noreply.github.com>
2022-12-28 13:28:11 -0500

    add nuxt-icon and start with toggles

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="w-full relative">
+    <ActionToggles />
     <AnimatedLink />
     <ProjectGrid />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,3 @@
 export default defineNuxtConfig({
-  modules: ['nuxt-windicss'],
+  modules: ['nuxt-windicss', 'nuxt-icon'],
 });
